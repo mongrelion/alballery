4dbdd294ae8b4ab72fb818c197fd3586bdc7adae
Carlos León <mail@carlosleon.info>
2012-05-07 20:07:24 -0500

    Updating views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def session_links
+    if user_signed_in?
+      link_to('Sign out', destroy_user_session_path(method: :delete))
+    else
+      raw(
+        link_to('Sign in', new_user_session_path) +
+        ' or ' +
+        link_to('Sign up', new_user_registration_path)
+      )
+    end
+  end
 end
